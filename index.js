3e1198e48bb8f359983d3991021d0866103467ed
Jearon Danglade <jdanglade82@gmail.com>
2026-06-09 13:49:09 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 84 / 2 - 2;
+
+let code3 = 12 * 3 + (15 % 6);
+
+let message = "The vault has been secured, the combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message, codeA, codeB);
